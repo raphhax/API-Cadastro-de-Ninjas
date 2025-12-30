2f6791ca92b11e45df0eff034c14ce3f4640d761
raphhax <raphaelhenriquedev@gmail.com>
2025-12-29 21:36:49 -0300

    Commit inicial

---
+++ src/test/java/dev/raphhax/CadastroDeNinja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raphhax.CadastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
