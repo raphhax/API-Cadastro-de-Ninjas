82f51ce301b25bc7454e6cf6dc3865927cb39ff2
raphhax <raphaelhenriquedev@gmail.com>
2025-12-29 23:04:15 -0300

    Rodando o primeiro programa na API.

--- src/main/java/dev/raphhax/CadastroDeNinja/Controller.java
+++ src/main/java/dev/raphhax/CadastroDeNinja/Controller.java
@@ -1,5 +1,6 @@
 package dev.raphhax.CadastroDeNinja;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Controller {
 
+    @GetMapping ("/boasVindas")
+    public String boasVindas(){
+        return "Minha primeira mensagem.";
+    }
 }
