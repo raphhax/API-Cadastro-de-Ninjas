46544ebd96285621f55507eaee11dc1d04d267d9
raphhax <raphaelhenriquedev@gmail.com>
2025-12-30 00:46:16 -0300

    Add atributos, construtores, getters e setters.

---
+++ src/main/java/dev/raphhax/CadastroDeNinja/NinjaModel.java
@@ -0,0 +1,44 @@
+package dev.raphhax.CadastroDeNinja;
+
+public class NinjaModel {
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+
+}
