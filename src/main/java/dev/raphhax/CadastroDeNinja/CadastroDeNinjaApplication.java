0de52e7abe4614782a85b10025c857d1f20df07c
raphhax <raphaelhenriquedev@gmail.com>
2026-01-01 03:01:02 -0300

    Docs: Primeiro commit de 2026. Apenas um comentario sobre isso...

--- src/main/java/dev/raphhax/CadastroDeNinja/CadastroDeNinjaApplication.java
+++ src/main/java/dev/raphhax/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -1,5 +1,7 @@
 package dev.raphhax.CadastroDeNinja;
 
+// FIRST COMMIT FROM 2026 HEHEHEHHEHE LETS GO
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
