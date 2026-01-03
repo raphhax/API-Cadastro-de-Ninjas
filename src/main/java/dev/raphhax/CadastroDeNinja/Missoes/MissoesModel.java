44efdca548decf9a57a2251384a7bc0d05efcc2a
raphhax <raphaelhenriquedev@gmail.com>
2026-01-03 18:06:23 -0300

    feat: Adiciona construtores lombok nas missoes.

--- src/main/java/dev/raphhax/CadastroDeNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/raphhax/CadastroDeNinja/Missoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.raphhax.CadastroDeNinja.Missoes;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.raphhax.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity //tudo abaixo eh uma entidade
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -22,9 +28,5 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") // Uma missao pode ter varios ninjas
     @JsonIgnore // ignorar possivel problema de lista infinita na serializacao
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
 
 }
